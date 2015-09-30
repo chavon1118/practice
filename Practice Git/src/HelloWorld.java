28550d117450efdc7173a5632b0b5b9a0fd3e83b
chavon1118 <chavonzhou@gmail.com>
2015-09-30 00:29:25 -0700

    added second line

--- Practice Git/src/HelloWorld.java
+++ Practice Git/src/HelloWorld.java
@@ -2,5 +2,6 @@
 public class HelloWorld {
 	public static void main (String[] args){
 		System.out.println("This is a hello world for git");
+		System.out.println("This is the second commit");
 	}
 }
